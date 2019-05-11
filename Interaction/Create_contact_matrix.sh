fe124da6767ec7ee66b2a03afee84e9556195287
Dongsung Lee <eaststar0@gmail.com>
2019-05-10 18:41:38 -0700

    Update and rename Create_supermatrix.sh to Create_contact_matrix.sh

--- Interaction/Create_contact_matrix.sh
+++ Interaction/Create_contact_matrix.sh
@@ -1,4 +1,4 @@
-#Usage: sh 100kb_Super_matrix_generate.sh input reference_chrNameLength_file             
+#Usage: sh Create_contact_matrix.sh input reference_chrNameLength_file             
 cooler makebins reference_chrNameLength_file ${2} 10000 > ${2}_10kb_bin.txt
 cooler csort --nproc 2 -c1 2 -p1 3 -c2 4 -p2 5 -o ${1}.sorted ${1} ${2}_10kb_bin.txt
 cooler cload pairix ${3}  ${1}.sorted ${1}_10kb_contacts.cool
