af239a2b936271e4ac6c40e72a9bcea0f0c161e7
Dongsung Lee <eaststar0@gmail.com>
2019-05-10 18:37:54 -0700

    Update Remove_missing_value.py

--- Methylation/Remove_missing_value.py
+++ Methylation/Remove_missing_value.py
@@ -1,7 +1,10 @@
+#Description: Remove rows with more than X number of 'NA'
+#Usage: python Remove_missing_value.py <input> <row with number of NA to remove>
 import sys
+X=sys.argv[2]
 dfh=open(sys.argv[1],'r')
 rfh=open(sys.argv[1]+'_All_covered.txt','w')
 for i in dfh:
         line=i.split()
-        if line.count('NA')==0:             
+        if line.count('NA')>=int(X):
                 rfh.write(i)
