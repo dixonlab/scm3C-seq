d927dba33a1f6fea0c6df9376139ea9295276c1f
Dongsung Lee <eaststar0@gmail.com>
2019-05-12 00:25:50 -0700

    Update Join_two_allc.sh

--- Join_two_allc.sh
+++ Join_two_allc.sh
@@ -1,4 +1,4 @@
 python allc_modifier.py ${1} &
 python allc_modifier.py ${2} &
 wait
-python Join_two_modified_allc.py ${1}_CpG_mod.txt ${2}_CpG_mod.txt ${3}
+python Join_two_modified_files_to_one.py ${1}_CpG_mod.txt ${2}_CpG_mod.txt ${3}
